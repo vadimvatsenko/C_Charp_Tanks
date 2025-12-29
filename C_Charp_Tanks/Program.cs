66330d372ba629cd07428d550bc9f53daf84d06e
Vadym <69381493+vadimvatsenko@users.noreply.github.com>
2025-12-29 15:22:40 +0200

    add Dict

--- C_Charp_Tanks/Program.cs
+++ C_Charp_Tanks/Program.cs
@@ -4,9 +4,9 @@ using C_Charp_Tanks.Window;
 public partial class Program
 {
     const float targetFrameTime = 1f / 60f;
-
     public static void Main(string[] args)
     {
+       
         WindowSettings windowSettings = new WindowSettings(90, 41);
 
         Console.OutputEncoding = System.Text.Encoding.UTF8;
